851a4100bd7d887ceeef4835cfadcfd01fe4dc64
EdwinTi <e.tijaro@uniandes.edu.co>
2023-05-06 14:45:19 -0500

    Template para iniciar pruebas e2e

--- Pruebas e2e con Cypress/cypress/e2e/monkey/Login.cy.js
+++ Pruebas e2e con Cypress/cypress/e2e/monkey/Login.cy.js
@@ -4,13 +4,26 @@
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Login con campo vacio
-describe('Inicio de sesión con campo vacio', () => {
+describe('Inicio de sesión con campos vacios', () => {
     it('Debe generar alerta al iniciar sesión con credenciales vacias', () => {
       cy.login(' ', ' ')
       cy.get('p.main-error').should('contain','Please fill out the form to sign in.')// Verifica que el usuario ha iniciado sesión correctamente
     })
   })
   
+//Login con credenciales incorrectas
+  describe('Inicio de sesión con credenciales incorrectas', () => {
+    it('Debe generar alerta al iniciar sesión con correo incorrecto', () => {
+      cy.login('David@uniandes.edu.co', 'Contrasena_123')
+      cy.get('p.main-error').should('contain','There is no user with that email address.')// Verifica que el usuario ha iniciado sesión correctamente
+    })
+    it('Debe generar alerta al iniciar sesión con conrtaseña incorrecto', () => {
+      cy.login('e.tijaro@uniandes.edu.co', '12345')
+      cy.get('p.main-error').should('contain','Your password is incorrect.')// Verifica que el usuario ha iniciado sesión correctamente
+    })
+  })
+
+
 //Login correcto 
 describe('Inicio de sesión', () => {
     it('Debe permitir iniciar sesión con credenciales válidas', () => {
